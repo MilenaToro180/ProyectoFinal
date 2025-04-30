b937aa412265f3e8f4d242687ec1e80dc58a7ca5
Adriana Milena Toro Villalobos <torovillalobosmilena@gmail.com>
2025-04-30 16:03:48 -0500

    v.07

---
+++ docs/programa.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([4.443829288974173, -74.04701794059552], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
