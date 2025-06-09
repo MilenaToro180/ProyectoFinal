f085dff3399458837aab62a9175242742e1efb61
torovillalobosmilena@gmail.com <torovillalobosmilena@gmail.com>
2025-06-09 15:53:31 -0500

    v.04

--- proyectochipaque/frontend/programa.js
+++ proyectochipaque/frontend/programa.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([4.443626718026901, -74.04593731929668], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -6,8 +7,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // click mapa
 
 function onMapClick(e) {
-   var marker = L.marker([e.latlng]).addTo(map);
-    
+   var marker = L.marker(e.latlng).addTo(map);    
 }
 
 map.on('click', onMapClick);
