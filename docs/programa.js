67a7badeae1e13251c5d1ec3f6c06da07373aea8
Adriana Milena Toro Villalobos <torovillalobosmilena@gmail.com>
2025-06-19 21:54:02 -0500

    v.09

--- docs/programa.js
+++ docs/programa.js
@@ -1,8 +1,12 @@
-var map = L.map('map').setView([4.443829288974173, -74.04701794059552], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let miObjeto=document.getElementById("MiEtiqueta")
-miObjeto.innerHTML="ig: @mileettt";
+//Manejar el evento de hacer click en el mapa
+
+    function onMapClick(e) {
+        var marker = L.marker(e.latlng).addTo(map);
+    }
+
+map.on('click', onMapClick);
